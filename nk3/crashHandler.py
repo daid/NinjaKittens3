cfc8026c89960578a946f4d730121f685b4b323b
daid <daid303@gmail.com>
2019-03-16 22:04:37 +0100

    Make the crash handler work if the crash was on a different thread.

--- nk3/crashHandler.py
+++ nk3/crashHandler.py
@@ -1,37 +1,47 @@
 import sys
 import traceback as traceback_module
-from typing import Type
+import threading
+from typing import Type, Optional
 from types import TracebackType
 
-from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QUrl, pyqtSignal
 from PyQt5.QtQml import QQmlApplicationEngine
 from nk3.logging import LogHandler
+from nk3.qt.QObjectBase import QObjectBase, qtSlot
 
 
-class CrashHandler:
+class CrashHandler(QObjectBase):
     _original_excepthook = None
     _instance = None
+    _main_thread = None
 
     @staticmethod
     def register() -> None:
         CrashHandler._original_excepthook = sys.excepthook
-        sys.excepthook = CrashHandler.handleException
+        CrashHandler._instance = CrashHandler()
+        sys.excepthook = CrashHandler._instance.handleException
+        _main_thread = threading.current_thread()
 
-    @staticmethod
-    def handleException(type_: Type[BaseException], value: BaseException, traceback: TracebackType) -> None:
-        CrashHandler._instance = CrashHandler(type_, value, traceback)
-        CrashHandler._instance.show()
+    def __init__(self) -> None:
+        super().__init__()
+        self.__crash_info = ""
+        self.__qml_engine = None  # type: Optional[QQmlApplicationEngine]
+        self.onShow.connect(self.show)
 
-    def __init__(self, type_: Type[BaseException], value: BaseException, traceback: TracebackType) -> None:
+    def handleException(self, type_: Type[BaseException], value: BaseException, traceback: TracebackType) -> None:
         self.__crash_info = "--------Exception--------\n"
         self.__crash_info += "".join(traceback_module.format_exception(type_, value, traceback))
         self.__crash_info += "--------LOG--------\n"
         for message in LogHandler.getLogHistory():
             self.__crash_info += "%s\n" % (message)
-        self.__qml_engine = QQmlApplicationEngine()
+        self.onShow.emit()
         if CrashHandler._original_excepthook is not None:
             CrashHandler._original_excepthook(type_, value, traceback)
 
+    onShow = pyqtSignal()
+
+    @qtSlot
     def show(self) -> None:
+        self.__qml_engine = QQmlApplicationEngine()
         self.__qml_engine.rootContext().setContextProperty("crash_info", self.__crash_info)
         self.__qml_engine.load(QUrl("resources/qml/Crash.qml"))
