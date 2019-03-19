6e8d9a6584fa5881e7da24ded2dc0fce5a644d72
daid <daid303@gmail.com>
2019-03-19 09:52:46 +0100

    Fix type hints

--- nk3/processor/result.py
+++ nk3/processor/result.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple
+from typing import NamedTuple, List
 
 from nk3.processor.pathUtils import Path
 
@@ -7,13 +7,13 @@ Move = NamedTuple('Move', [('xy', complex), ('z', float), ('speed', float)])
 
 class Result:
     def __init__(self):
-        self.__moves = []
+        self.__moves = []  # type: List[Move]
 
-    def addMove(self, move: Move):
+    def addMove(self, move: Move) -> None:
         self.__moves.append(move)
 
-    def addProblemRegion(self, path: Path):
+    def addProblemRegion(self, path: Path) -> None:
         pass
 
-    def addProblemPath(self, path: Path):
+    def addProblemPath(self, path: Path) -> None
         pass
